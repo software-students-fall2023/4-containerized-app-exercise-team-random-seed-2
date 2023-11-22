92aa7b7b200f823ba1d90393f83f055cdf044699
larahynkim <lara.kimcf@gmail.com>
2023-11-22 19:11:42 +0900

    format with black

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -10,21 +10,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-IBM_API_URL = os.getenv('API_BASE_URL')
-IBM_API_KEY = os.getenv('IBM_API_KEY')
+IBM_API_URL = os.getenv("API_BASE_URL")
+IBM_API_KEY = os.getenv("IBM_API_KEY")
 
 authenticator = IAMAuthenticator(IBM_API_KEY)
 speech_to_text = SpeechToTextV1(authenticator=authenticator)
 speech_to_text.set_service_url(IBM_API_URL)
 
+
 def transcribe_audio(wave_output_filename):
     """Transcribe the audio file using IBM Watson Speech to Text."""
     try:
-        with open(wave_output_filename, 'rb') as audio_file:
+        with open(wave_output_filename, "rb") as audio_file:
             response = speech_to_text.recognize(
-                audio=audio_file,
-                content_type='audio/wav',
-                model='en-US_BroadbandModel'
+                audio=audio_file, content_type="audio/wav", model="en-US_BroadbandModel"
             ).get_result()
             return json.dumps(response, indent=2)
     except FileNotFoundError as e:
@@ -34,25 +33,23 @@ def transcribe_audio(wave_output_filename):
         print(f"JSON decode error: {e}")
         return None
 
+
 def save_transcription_to_mongodb(transcription, filename):
     """Save the transcription to MongoDB."""
     try:
-        client = MongoClient('mongodb://host.docker.internal:27017/')
+        client = MongoClient("mongodb://host.docker.internal:27017/")
         db = client.transcription_database
         collection = db.transcriptions
         transcription_document = {
             "text": transcription,
             "timestamp": datetime.now(),
-            "metadata": {
-                "duration": 5,  
-                "source": "file",
-                "filename": filename
-            }
+            "metadata": {"duration": 5, "source": "file", "filename": filename},
         }
         collection.insert_one(transcription_document)
         print(f"Transcription of {filename} saved to MongoDB.")
     finally:
         client.close()
+
 
 def process_audio_files(directory):
     """Process all WAV audio files in the given directory."""
@@ -64,10 +61,12 @@ def process_audio_files(directory):
                 print(f"Transcription of {filename}: \n{transcription_result}")
                 save_transcription_to_mongodb(transcription_result, filename)
 
+
 def main():
     """Process audio files in the directory."""
     audio_files_dir = "audio_files"
     process_audio_files(audio_files_dir)
+
 
 if __name__ == "__main__":
     main()
