62ae2b85fd5e4e5596fe7a19cefec49fbe9bd6cd
larahynkim <lara.kimcf@gmail.com>
2023-12-04 14:52:09 +0900

    finish formatting

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -20,14 +20,15 @@ authenticator = IAMAuthenticator(IBM_API_KEY)
 speech_to_text = SpeechToTextV1(authenticator=authenticator)
 speech_to_text.set_service_url(IBM_API_URL)
 
-@app.route('/transcribe', methods=['POST'])
+
+@app.route("/transcribe", methods=["POST"])
 def transcribe():
     """Handle audio file transcription request."""
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
 
     try:
         transcription_result = transcribe_audio(file)
@@ -35,27 +36,27 @@ def transcribe():
             transcription_document = save_transcription_to_mongodb(
                 transcription_result, file.filename
             )
-            return jsonify({'transcription': transcription_document}), 200
-        return jsonify({'error': 'Transcription failed'}), 500
+            return jsonify({"transcription": transcription_document}), 200
+        return jsonify({"error": "Transcription failed"}), 500
     except ApiException as e:
         app.logger.error("IBM Watson API error: %s: %s", e.code, e.message)
-        return jsonify({'error': 'Error with IBM Watson service'}), 500
+        return jsonify({"error": "Error with IBM Watson service"}), 500
     except PyMongoError as e:
         app.logger.error("MongoDB error: %s", str(e))
-        return jsonify({'error': 'Error saving data to database'}), 500
+        return jsonify({"error": "Error saving data to database"}), 500
+
 
 def transcribe_audio(file):
     """Transcribe the audio file using IBM Watson."""
     try:
         response = speech_to_text.recognize(
-            audio=file.stream,
-            content_type='audio/wav'
+            audio=file.stream, content_type="audio/wav"
         ).get_result()
         return response
     except ApiException as e:
-        app.logger.error("IBM Watson API error: %s: %s",
-                         e.code, e.message)
+        app.logger.error("IBM Watson API error: %s: %s", e.code, e.message)
         return None
+
 
 def save_transcription_to_mongodb(transcription, filename):
     """Save transcription result to MongoDB."""
@@ -64,19 +65,24 @@ def save_transcription_to_mongodb(transcription, filename):
     collection = db.transcriptions
     try:
         transcription_text = "No transcript available."
-        if transcription.get('results', []) and transcription['results'][0].get('alternatives', []):
-            transcription_text = transcription['results'][0]['alternatives'][0]['transcript']
+        if transcription.get("results", []) and transcription["results"][0].get(
+            "alternatives", []
+        ):
+            transcription_text = transcription["results"][0]["alternatives"][0][
+                "transcript"
+            ]
         transcription_document = {
             "filename": filename,
             "transcript": transcription_text,
-            "timestamp": datetime.now()
+            "timestamp": datetime.now(),
         }
         insert_result = collection.insert_one(transcription_document)
-        transcription_document['_id'] = str(insert_result.inserted_id)
+        transcription_document["_id"] = str(insert_result.inserted_id)
         return transcription_document
     except PyMongoError as e:
         app.logger.error("MongoDB error: %s", str(e))
         return None
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=4000)
+    app.run(debug=True, host="0.0.0.0", port=4000)
--- web-app/app.py
+++ web-app/app.py
@@ -1,7 +1,8 @@
+# pylint: disable=import-error
 """Flask application for audio transcription."""
 from datetime import datetime
 import os
-from flask import Flask, render_template, request , flash
+from flask import Flask, render_template, request, flash
 import requests
 from pymongo import MongoClient
 from pymongo import errors as pymongo_errors
@@ -9,60 +10,70 @@ from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'
+app.secret_key = "secret_key"
 
 # MongoDB connection
 client = MongoClient("mongodb://mongodb:27017/")
 db = client.transcription_database
 collection = db.transcriptions
 
+
 def allowed_file(filename):
     """Check if the file is a .wav file."""
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'wav'
+    return "." in filename and filename.rsplit(".", 1)[1].lower() == "wav"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     """Display the main page with a list of transcriptions."""
     transcriptions_list = list(
-        collection.find({"transcript": {"$exists": True}}).sort("timestamp", -1))
-    return render_template('index.html', transcriptions=transcriptions_list)
-#@app.route('/record_audio')
-#def record_audio():
-   # return render_template('record_audio.html')
+        collection.find({"transcript": {"$exists": True}}).sort("timestamp", -1)
+    )
+    return render_template("index.html", transcriptions=transcriptions_list)
 
-@app.route('/upload_transcribe', methods=['GET', 'POST'])
+
+# @app.route('/record_audio')
+# def record_audio():
+# return render_template('record_audio.html')
+
+
+@app.route("/upload_transcribe", methods=["GET", "POST"])
 def upload_transcribe():
     """Handle the transcription upload and processing."""
-    if request.method == 'POST':
-        file = request.files.get('audiofile')
+    if request.method == "POST":
+        file = request.files.get("audiofile")
         if file and allowed_file(file.filename):
             response = send_file_to_ml_client(file)
             if response.status_code == 200:
                 latest_transcription = collection.find_one(sort=[("timestamp", -1)])
-                additional_data = collection.find_one({"file_name": secure_filename(file.filename)},
-                                                       sort=[("storage_time", -1)])
-                return render_template('transcription_result.html',
-                                       transcription=latest_transcription,
-                                       additional=additional_data)
-            else:
-                flash('Error processing the file. Please try again.', 'error')
+                additional_data = collection.find_one(
+                    {"file_name": secure_filename(file.filename)},
+                    sort=[("storage_time", -1)],
+                )
+                return render_template(
+                    "transcription_result.html",
+                    transcription=latest_transcription,
+                    additional=additional_data,
+                )
+            flash("Error processing the file. Please try again.", "error")
         else:
-            flash('Please upload a .wav audio file.', 'error')
+            flash("Please upload a .wav audio file.", "error")
 
-    return render_template('upload_transcribe.html')
+    return render_template("upload_transcribe.html")
+
 
 def send_file_to_ml_client(file):
     """Send file to the machine learning client for processing."""
     temp_filename = secure_filename(file.filename)
     file.save(temp_filename)
     save_additional_data_to_mongodb(temp_filename)
-    with open(temp_filename, 'rb') as f:
+    with open(temp_filename, "rb") as f:
         response = requests.post(
-            'http://ml-client:4000/transcribe',
-            files={'file': f},
-            timeout=20)
+            "http://ml-client:4000/transcribe", files={"file": f}, timeout=20
+        )
     os.remove(temp_filename)
     return response
+
 
 def save_additional_data_to_mongodb(file_name):
     """Save additional data to the MongoDB database."""
@@ -71,11 +82,12 @@ def save_additional_data_to_mongodb(file_name):
         additional_data = {
             "file_name": file_name,
             "file_path": file_path,
-            "storage_time": datetime.now()   
+            "storage_time": datetime.now(),
         }
         collection.insert_one(additional_data)
     except pymongo_errors.PyMongoError as e:
-        app.logger.error(f"MongoDB error: {type(e).__name__}: {str(e)}")
+        app.logger.error("MongoDB error: %s: %s", type(e).__name__, str(e))
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5001)
