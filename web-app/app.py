6393be1d12272eea63873121f44133055198bb23
fishlesswater <henrywang1030@outlook.com>
2023-12-03 23:05:05 -0500

    linting2

--- web-app/app.py
+++ web-app/app.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask, render_template, request , flash
 import requests
 from pymongo import MongoClient
+from pymongo import errors as pymongo_errors
 from werkzeug.utils import secure_filename
 
 
@@ -25,7 +26,6 @@ def index():
     transcriptions_list = list(
         collection.find({"transcript": {"$exists": True}}).sort("timestamp", -1))
     return render_template('index.html', transcriptions=transcriptions_list)
-
 #@app.route('/record_audio')
 #def record_audio():
    # return render_template('record_audio.html')
@@ -39,13 +39,15 @@ def upload_transcribe():
             response = send_file_to_ml_client(file)
             if response.status_code == 200:
                 latest_transcription = collection.find_one(sort=[("timestamp", -1)])
-                additional_data = collection.find_one({"file_name": secure_filename(file.filename)}, sort=[("storage_time", -1)])
-                return render_template('transcription_result.html', transcription=latest_transcription, additional=additional_data)
+                additional_data = collection.find_one({"file_name": secure_filename(file.filename)},
+                                                       sort=[("storage_time", -1)])
+                return render_template('transcription_result.html', 
+                                       transcription=latest_transcription, additional=additional_data)
             else:
                 flash('Transcription failed')
         else:
             flash('No file selected')
-    return render_template('upload_transcribe.html')
+    
 
 
 def send_file_to_ml_client(file):
@@ -55,10 +57,10 @@ def send_file_to_ml_client(file):
     save_additional_data_to_mongodb(temp_filename)
     with open(temp_filename, 'rb') as f:
         response = requests.post(
-            'http://ml-client:4000/transcribe', 
+            'http://ml-client:4000/transcribe',
             files={'file': f},
             timeout=20)
-    os.remove(temp_filename) 
+    os.remove(temp_filename)
     return response
 
 def save_additional_data_to_mongodb(file_name):
@@ -71,8 +73,8 @@ def save_additional_data_to_mongodb(file_name):
             "storage_time": datetime.now()        
         }
         collection.insert_one(additional_data)
-    except Exception as e:
-        app.logger.error(f"Error saving additional data to MongoDB: {type(e).__name__}: {str(e)}")
+    except pymongo_errors.PyMongoError as e:
+        app.logger.error(f"MongoDB error: {type(e).__name__}: {str(e)}")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
